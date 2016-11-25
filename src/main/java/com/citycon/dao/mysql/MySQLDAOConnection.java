2a5d01662050dadf7d08547f0bdbb870d31496b7
DESKTOP-EU7S98I\Vojts <vojtseh2014@gmail.com>
2016-11-26 02:07:15 +0300

    Revert "Connection pool"
    This reverts commit 14243681858b3e310f105ad85b349ea3ca8cf58c.

--- src/main/java/com/citycon/dao/mysql/MySQLDAOConnection.java
+++ src/main/java/com/citycon/dao/mysql/MySQLDAOConnection.java
@@ -1,13 +1,79 @@
+//package com.citycon.dao.mysql;
+//
+//import com.citycon.dao.exceptions.InternalDAOException;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import javax.naming.InitialContext;
+//import javax.naming.NamingException;
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+///**
+// * Created by Vojts on 13.11.2016.
+// */
+//public class MySQLDAOConnection {
+//
+//    private static volatile MySQLDAOConnection instance;
+//
+//    private static DataSource dataSource;
+//
+//    private Logger logger;
+//
+//    /**
+//     * @throws InternalDAOException
+//     */
+//    private MySQLDAOConnection() throws InternalDAOException {
+//        logger = LoggerFactory.getLogger("com.citycon.dao.mysql.MySQLDAOConnection");
+//
+//        try {
+//            InitialContext ctx = new InitialContext();
+//            logger.trace("Get context", ctx);
+//            dataSource = (DataSource) ctx.lookup("jdbc/City");
+//        } catch (NamingException e) {
+//            logger.error("ConnectionPool create failed", e);
+//            throw new InternalDAOException(e);
+//        }
+//
+//        logger.trace("ConnectionPool create");
+//    }
+//
+//    /**
+//     * @throws InternalDAOException
+//     */
+//    public static MySQLDAOConnection getInstance() throws InternalDAOException {
+//        MySQLDAOConnection localInstance = instance;
+//        if (localInstance == null) {
+//            synchronized (MySQLDAOConnection.class) {
+//                localInstance = instance;
+//                if (localInstance == null) {
+//                    instance = localInstance = new MySQLDAOConnection();
+//                }
+//            }
+//        }
+//        return localInstance;
+//    }
+//
+//    public Connection getConnection() throws InternalDAOException {
+//        try {
+//            logger.trace("getConnection");
+//            return dataSource.getConnection();
+//        } catch (SQLException e) {
+//            logger.error("Get connection failed", e);
+//            throw new InternalDAOException("Get connection failed", e);
+//        }
+//    }
+//}
+
 package com.citycon.dao.mysql;
 
 import com.citycon.dao.exceptions.InternalDAOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -17,7 +83,13 @@ public class MySQLDAOConnection {
 
     private static volatile MySQLDAOConnection instance;
 
-    private static DataSource dataSource;
+    private ConnectionPool connectionPool;
+
+    private final String MYSQL_CONNECTOR_CLASS = "com.mysql.jdbc.Driver";
+
+    private static final String URL = "jdbc:mysql://23.99.115.175:3306/nc2_cityCon";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "nc_2groupDB";
 
     private Logger logger;
 
@@ -26,17 +98,15 @@ public class MySQLDAOConnection {
      */
     private MySQLDAOConnection() throws InternalDAOException {
         logger = LoggerFactory.getLogger("com.citycon.dao.mysql.MySQLDAOConnection");
-
         try {
-            InitialContext ctx = new InitialContext();
-            logger.trace("Get context", ctx);
-            dataSource = (DataSource) ctx.lookup("jdbc/City");
-        } catch (NamingException e) {
-            logger.error("ConnectionPool create failed", e);
-            throw new InternalDAOException(e);
+            Class.forName(MYSQL_CONNECTOR_CLASS);
+            connectionPool = new ConnectionPool(URL, USERNAME, PASSWORD);
+            connectionPool.setMaxUseTime(3*1000);
+            logger.trace("ConnectionPool create");
+        } catch (ClassNotFoundException e) {
+            logger.error("Driver for database failed");
+            throw new InternalDAOException("Driver for database failed", e);
         }
-
-        logger.trace("ConnectionPool create");
     }
 
     /**
@@ -57,11 +127,11 @@ public class MySQLDAOConnection {
 
     public Connection getConnection() throws InternalDAOException {
         try {
-            logger.trace("getConnection");
-            return dataSource.getConnection();
+            return connectionPool.getConnection();
         } catch (SQLException e) {
             logger.error("Get connection failed", e);
             throw new InternalDAOException("Get connection failed", e);
         }
     }
+
 }
