1293f10f54bcdcb72efd875a984cd903725ff1cb
DESKTOP-EU7S98I\Vojts <vojtseh2014@gmail.com>
2016-11-25 16:50:42 +0300

    connection pool via glassfish

--- src/main/java/com/citycon/dao/mysql/MySQLDAOConnection.java
+++ src/main/java/com/citycon/dao/mysql/MySQLDAOConnection.java
@@ -4,8 +4,11 @@ import com.citycon.dao.exceptions.InternalDAOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -15,13 +18,7 @@ public class MySQLDAOConnection {
 
     private static volatile MySQLDAOConnection instance;
 
-    private ConnectionPool connectionPool;
-
-    private final String MYSQL_CONNECTOR_CLASS = "com.mysql.jdbc.Driver";
-
-    private static final String URL = "jdbc:mysql://23.99.115.175:3306/nc2_cityCon";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "nc_2groupDB";
+    private static DataSource dataSource;
 
     private Logger logger;
 
@@ -30,15 +27,18 @@ public class MySQLDAOConnection {
      */
     private MySQLDAOConnection() throws InternalDAOException {
         logger = LoggerFactory.getLogger("com.citycon.dao.mysql.MySQLDAOConnection");
+
         try {
-            Class.forName(MYSQL_CONNECTOR_CLASS);
-            connectionPool = new ConnectionPool(URL, USERNAME, PASSWORD);
-            connectionPool.setMaxUseTime(30*1000);
-            logger.trace("ConnectionPool create");
-        } catch (ClassNotFoundException e) {
-            logger.error("Driver for database failed");
-            throw new InternalDAOException("Driver for database failed", e);
+            InitialContext initialContext = new InitialContext();
+            Context ctx = (Context) initialContext;
+            logger.trace("Get context", ctx);
+            dataSource = (DataSource) ctx.lookup("jdbc/City");
+        } catch (NamingException e) {
+            logger.error("ConnectionPool create failed", e);
+            throw new InternalDAOException(e);
         }
+
+        logger.trace("ConnectionPool create");
     }
 
     /**
@@ -59,7 +59,8 @@ public class MySQLDAOConnection {
 
     public Connection getConnection() throws InternalDAOException {
         try {
-            return connectionPool.getConnection();
+            logger.trace("getConnection");
+            return dataSource.getConnection();
         } catch (SQLException e) {
             logger.error("Get connection failed", e);
             throw new InternalDAOException("Get connection failed", e);
