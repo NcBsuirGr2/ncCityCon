72ed3798dbd04a6069224a11cd6ba70baa57c62b
Mike <zzoxall@gmail.com>
2016-12-03 14:24:36 +0300

    GetAllUsers service

---
+++ src/main/java/com/citycon/services/GetAllUsers.java
@@ -0,0 +1,61 @@
+package com.citycon.services;
+
+import com.citycon.dao.exceptions.InternalDAOException;
+import com.citycon.dao.exceptions.InvalidDataDAOException;
+import com.citycon.model.systemunits.entities.UserEntity;
+import com.citycon.services.data.User;
+import com.citycon.dao.mysql.UserDAO;
+
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL)
+public class GetAllUsers {
+    @WebMethod
+	public String getTestString(){
+	    return "test string from cityCon app";
+    }
+    @WebMethod
+    public String greet(String name){
+        return "Nice to see you "+name;
+    }
+    @WebMethod
+    public User getTestUser(){
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        user.setPassword("testpass");
+        user.setLogin("testuser");
+        return user;
+    }
+    @WebMethod
+    public User getUserWithLogin(String login){
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        user.setPassword("testpass");
+        user.setLogin(login);
+        return user;
+    }
+//
+    @WebMethod
+    public @WebResult(name = "user") User[] getUsers(){
+        User returnUsers[] = null;
+        try {
+            UserDAO dao = UserDAO.getInstance();
+            UserEntity users[] = dao.getPage(1, 999, "login", true);
+            returnUsers = new User[users.length];
+            for (int i = 0; i < users.length; ++i) {
+                returnUsers[i] = new User();
+                returnUsers[i].setLogin(users[i].getLogin());
+                returnUsers[i].setEmail(users[i].getEmail());
+                returnUsers[i].setPassword(users[i].getPassword());
+            }
+        } catch(InternalDAOException | InvalidDataDAOException e) {
+            // TODO: return null
+        }
+        return returnUsers;
+//    Вадим
+    }
+}
---
+++ src/main/java/com/citycon/services/data/User.java
@@ -0,0 +1,35 @@
+package com.citycon.services.data;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "user")
+@XmlType(propOrder = {"login", "password", "email"})
+public class User {
+    private String login;
+    private String password;
+    private String email;
+
+    public String getLogin() {
+        return login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    @XmlElement(name = "login")
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    @XmlElement(name = "password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @XmlElement(name = "email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
